aa36ecb5139db38debe786b526710d4a49896a88
Davor Rapić <rapic.davor@gmail.com>
2020-05-11 10:05:02 +0200

    sass support, concurently and some express middleware

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
+var morgan = require('morgan');
+var cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
-
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
 
 module.exports = app;
