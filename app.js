41e3fbbd359f011f298699a61e5d4bce284e0f13
Davor Rapić <rapic.davor@gmail.com>
2020-05-11 12:18:04 +0200

    not found and error middleware

--- app.js
+++ app.js
@@ -11,4 +11,29 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(helmet());
 
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'Response from the Express API',
+    });
+});
+
+// 404 not found
+app.use((req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+});
+
+// error handler
+app.use((error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? 'Contact your sys admin for more details' : error.stack
+    })
+})
+
+
 module.exports = app;
